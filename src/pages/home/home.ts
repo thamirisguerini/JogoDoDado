a3001182896be4557f4b47b6a09828b95f214c49
Thamiris <thamirisaguerini@gmail.com>
2019-03-24 01:00:45 -0300

    concluido

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,87 @@
 import { Component } from '@angular/core';
-import { NavController} from 'ionic-angular';
+import { NavController, AlertController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  myrand:number;
-  num:string;
-  constructor(public navCtrl: NavController) {
+
+  numero:string;
+
+  constructor(public navCtrl: NavController, public alertCrtl: AlertController) {}
+
+    gerandojogo(){
+
+      var sorteando = Math.floor(Math.random() * 6 + 1);
+
+      switch(sorteando){
+
+        case 1:{
+          var lado1 = (<HTMLInputElement>document.getElementById("face")).src ="./assets/imgs/dado1.png";
+          break;
+        }
+          case 2:{
+          var lado2 = (<HTMLInputElement>document.getElementById("face")).src ="./assets/imgs/dado2.png";
+          break;
+          }
+          case 3:{
+          var lado3 = (<HTMLInputElement>document.getElementById("face")).src ="./assets/imgs/dado3.png";
+          break;
+          }
+          case 4:{
+          var lado4 = (<HTMLInputElement>document.getElementById("face")).src ="./assets/imgs/dado4.png";
+          break;
+          }
+          case 5:{
+          var lado5 = (<HTMLInputElement>document.getElementById("face")).src ="./assets/imgs/dado5.png";
+          break;
+          }
+          case 6:{
+          var lado6 = (<HTMLInputElement>document.getElementById("face")).src ="./assets/imgs/dado6.png";
+          break;
+          }
+          default:{
+            alert("Escolha um número!");
+          break;
+          }
+      }
+
+      let resultado = parseInt(this.numero)
+
+      if(resultado == sorteando)
+      {
+        this.Ganhou(resultado.toString());
+      }
+      else{
+        this.Perdeu(resultado.toString());
+      }
+
+    }
+
+    Ganhou(valor: String){
+
+      let alert = this.alertCrtl.create({
+      title: 'Você Acertou!',
+      subTitle: `Parabéns: ${valor}`,
+      buttons:['Jogue Novamente']
+      });
+
+    alert.present();
   }
 
-  jogar(){
-   this.myrand = this.random();
+  Perdeu(valor: String){
 
+    let alert = this.alertCrtl.create({
+    title: 'Você Errou!',
+    subTitle: `Tente Novamente: ${valor}`,
+    buttons:['Jogue Novamente']
+    });
+
+  alert.present();
   }
-  random(): number {
-    let rand = Math.floor(Math.random()*6)+1;
-    return rand;
-  }
+
+
 
 }
+
